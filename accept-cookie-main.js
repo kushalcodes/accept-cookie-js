7d571f96e907719002e127a6ae85cb96a62db3a0
kushalcodes <iamkushalneupane@gmail.com>
2021-05-09 01:41:40 +0545

    change

--- accept-cookie-main.js
+++ accept-cookie-main.js
@@ -1,15 +1,9 @@
-[
-  "https://cdn.jsdelivr.net/npm/js-cookie@rc/dist/js.cookie.min.js"
-].forEach(src => {
-  const script = document.createElement('script');
-  script.setAttribute('src', src);
-  document.head.appendChild(script);
-});
+const dependency = "https://cdn.jsdelivr.net/npm/js-cookie@rc/dist/js.cookie.min.js";
+const dependencyScript = document.createElement('script');
+dependencyScript.setAttribute('src', dependency);
+document.head.appendChild(dependencyScript);
 
-[
-  "https://cdn.jsdelivr.net/gh/kushalcodes/accept-cookie-js/accept-cookie-min.js"
-].forEach(src => {
-  const script = document.createElement('script');
-  script.setAttribute('src', src);
-  document.body.appendChild(script);
-});
+const lastCommit = "d7c0116a55b9bcf4277964873d4f73710306b624";
+const acceptCookieMinJs = document.createElement('script');
+acceptCookieMinJs.setAttribute('src', "https://cdn.jsdelivr.net/gh/kushalcodes/accept-cookie-js@" + lastCommit + "/accept-cookie-min.js");
+document.body.appendChild(acceptCookieMinJs);
